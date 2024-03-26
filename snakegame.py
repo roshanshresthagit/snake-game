dc9a7a512a215e7a15f756048cd9b533e8128a66
Roshan <roshanshrestha0921@gmail.com>
2024-03-26 21:02:20 +0545

    c1

--- snakegame.py
+++ snakegame.py
@@ -1,16 +1,46 @@
-import pygame,sys
+import pygame,sys,random
+from pygame.math import Vector2
+
+
+class Snake:
+    def __init__(self):
+        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+    
+    def draw_snake(self):
+        for block in self.body:
+            block_rect = pygame.Rect(block.x*cell_size,block.y*cell_size,cell_size,cell_size)
+            pygame.draw.rect(screen,(183,100,122),block_rect)
+
+class Fruit:
+    def __init__(self):
+        self.x=random.randint(0,cell_number-1)
+        self.y=random.randint(0,cell_number-1)
+        self.pos = Vector2(self.x,self.y)
+        #create an x and y position
+        #draw a square
+
+    def draw_fruit(self):
+        fruit_rect=pygame.Rect(self.pos.x*cell_size,self.pos.y*cell_size,cell_size,cell_size)
+        pygame.draw.rect(screen,(126,166,144),fruit_rect)
+
 
 pygame.init()#starts pygame
-screen = pygame.display.set_mode((400,500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
 clock = pygame.time.Clock()
-
-
+fruit = Fruit()
+snake= Snake()
 while True:
     #draw all our elements
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() 
             sys.exit()
+    screen.fill((175,215,70))
+    fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
-    clock.tick(framerate=60)
-    
+    clock.tick(60)
+
+ 
