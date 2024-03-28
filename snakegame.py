bf7f740de9c1666ece69591c94c603c7546d193e
Roshan <roshanshrestha0921@gmail.com>
2024-03-28 18:39:25 +0545

    snake graphics

--- snakegame.py
+++ snakegame.py
@@ -27,12 +27,48 @@ class Snake:
         self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
         self.crunch_sound = pygame.mixer.Sound('Sound/crunch.wav')
 
-
-    
     def draw_snake(self):
-        for block in self.body:
-            block_rect = pygame.Rect(block.x*cell_size,block.y*cell_size,cell_size,cell_size)
-            pygame.draw.rect(screen,(183,100,122),block_rect)
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
+
+        for index,block in enumerate(self.body):
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+
+            if index==0:
+                screen.blit(self.head_right,block_rect)
+            elif index == len(self.body)-1:
+                screen.blit(self.tail,block_rect)
+            else:
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:screen.blit(self.body_vertical,block_rect)
+                elif previous_block.y == next_block.y:screen.blit(self.body_horizontal,block_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:screen.blit(self.body_tl,block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:screen.blit(self.body_bl,block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:screen.blit(self.body_tr,block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:screen.blit(self.body_br,block_rect)
+
+    def update_head_graphics(self):
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == Vector2(1,0): self.head= self.head_left
+        elif head_relation == Vector2(-1,0): self.head = self.head_right
+        elif head_relation == Vector2(0,1): self.head = self.head_up
+        elif head_relation == Vector2(0,-1): self.head = self.head_down
+
+        
+    
+    def update_tail_graphics(self):
+        tail_relation=self.body[-2]-self.body[-1]
+        if tail_relation==Vector2(1,0): self.tail=self.tail_left
+        elif tail_relation==Vector2(-1,0): self.tail=self.tail_right
+        elif tail_relation==Vector2(0,1):self.tail=self.tail_up
+        elif tail_relation==Vector2(1,0):self.tail=self.tail_down
+        
+           
     
     def move_snake(self):
         if self.new_block == True:
@@ -98,10 +134,10 @@ class Main:
         sys.exit()
 
 
-
+pygame.mixer.pre_init(44100,-16,2,512)
 pygame.init()#starts pygame
-cell_size = 30
-cell_number = 15
+cell_size = 40
+cell_number = 20
 screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
 clock = pygame.time.Clock()
 apple = pygame.image.load('Graphics/apple.png').convert_alpha()
